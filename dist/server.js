a305897585c89cf93e580ce77670b37a889ada9d
chailee <leeangkit@gmail.com>
2017-07-24 23:36:22 +0700

    Modified dist

--- dist/server.js
+++ dist/server.js
@@ -10,6 +10,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'appid, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    } else {
+        next();
+    }
+});
 
 route.setRoute(app);
 
